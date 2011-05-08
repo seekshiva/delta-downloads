a245b66f4fd3df25620ede57bf2114f89de57af1
V. Shiva Nandan <seekshiva@gmail.com>
2011-05-08 13:12:00 +0530

    Updated the search icon and made the contents of the home page to be fetched dynamically from ajax.php using ajax

---
+++ ajax.php
@@ -0,0 +1,38 @@
+<?php
+
+$home_page_contents=<<<str
+
+<div class="panel">
+<a><div class="panel-title">Featured</div></a>
+<div class="panel-contents">
+hi
+</div>
+</div>
+
+<div class="panel">
+<a><div class="panel-title">Most Downloaded</div></a>
+<div class="panel-contents">
+hi
+</div>
+</div>
+
+<div class="panel">
+<a><div class="panel-title">Highest Rated</div></a>
+<div class="panel-contents">
+hi
+</div>
+</div>
+
+<div class="panel">
+<a><div class="panel-title">Recently Added</div></a>
+<div class="panel-contents">
+hi
+</div>
+</div>
+
+str;
+
+
+echo $home_page_contents;
+
+?>
